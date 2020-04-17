0700feaa5de656d4e3eec206049cf4e05f6a5e2f
nexryai <61890205+nexryai@users.noreply.github.com>
2020-04-17 19:55:32 +0900

    Add files via upload

--- xcmp_cmp.sh
+++ xcmp_cmp.sh
@@ -3,7 +3,6 @@
 cmp_file=$(zenity --file-selection --directory --title "圧縮するディレクトリを選んで下さい")
 filedir=$(zenity --file-selection --directory --title "圧縮後ファイルを置くディレクトリを選んで下さい")
 filename=$(zenity --entry --title "xcmp" --text "圧縮後のファイル名")
-zenity --info --title "準備完了" --text "処理が完了したら通知します。okで続行します。"
 zenity --progress --text "お待ちください..." --pulsate & mksquashfs $cmp_file ${filedir}/${filename}.xcmp  
 zenity --notification --text "処理が終わりました。"
 zenity --info --title "done" --text "処理が完了しました"
--- xcmp_uncmp.sh
+++ xcmp_uncmp.sh
@@ -2,7 +2,6 @@
 unzip_file=$(zenity --file-selection --text "解凍するファイルを選んで下さい。")
 unzip_dir=$(zenity --file-selection --directory --title "解凍したファイルを置くディレクトリを選んで下さい")
 cd $unzip_dir
-zenity --info --title "準備完了" --text "処理が完了したら通知します。okで続行します。"
 zenity --progress --text "お待ちください..." --pulsate & sasquatch $unzip_file
 zenity --notification --text "処理が終わりました。"
 zenity --info --title "done" --text "処理が完了しました"
